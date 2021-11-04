6fb6df11b2cd02f95f92b1d544452ddb3c507454
andrewgll <du.ngmajor233@gmail.com>
2021-11-04 22:01:26 +0200

    changes

--- Snake/Form1.Designer.cs
+++ Snake/Form1.Designer.cs
@@ -37,7 +37,7 @@ namespace Snake
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(932, 665);
+            this.ClientSize = new System.Drawing.Size(1776, 1508);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
--- Snake/Form1.cs
+++ Snake/Form1.cs
@@ -15,14 +15,28 @@ namespace Snake
         PictureBox fruit = new PictureBox();
         private int _width = 900;
         private int _height = 800;
-        private int _sizeOfSides = 10;
+        private int _sizeOfSides = 20;
         private int dirX, dirY;
         private int rX, rY;
+        private Label labelScore;
+        private int score = 0;
+
         private List<PictureBox> snake = new List<PictureBox>();
 
         public Form1()
         {
             InitializeComponent();
+            Text = "Snake";
+            Width = _width;
+            Height = _height;
+
+            _generateMap();
+
+            labelScore = new Label();
+            labelScore.Text = "Score: 0";
+            labelScore.Location = new Point(810, 10);
+            Controls.Add(labelScore);
+
             PictureBox cube = new PictureBox();
             cube.BackColor = Color.DarkOliveGreen;
             cube.Size = new Size( _sizeOfSides, _sizeOfSides);
@@ -45,6 +59,44 @@ namespace Snake
 
         }
 
+        private void checkBorders()
+        {
+            if(snake[0].Location.X < 0)
+            {
+                Controls.Remove(snake[0]);
+                snake[0].Location = new Point(_height - _sizeOfSides, snake[0].Location.Y);
+                Controls.Add(snake[0]);
+                dirX = -1;
+                dirY = 0;
+            }
+            else if(snake[0].Location.X > _height -  _sizeOfSides)
+            {
+                Controls.Remove(snake[0]);
+                snake[0].Location = new Point(0, snake[0].Location.Y);
+                Controls.Add(snake[0]);
+                dirX = 1;
+                dirY = 0;
+            }
+            else if (snake[0].Location.Y < 0)
+            {
+                Controls.Remove(snake[0]);
+                snake[0].Location = new Point(snake[0].Location.X, _height - _sizeOfSides);
+                Controls.Add(snake[0]);
+                dirY = -1;
+                dirX = 0;
+            }
+            else if (snake[0].Location.Y > _height -  _sizeOfSides)
+            {
+                Controls.Remove(snake[0]);
+                snake[0].Location = new Point(snake[0].Location.X, 0);
+                Controls.Add(snake[0]);
+                dirY = 1;
+                dirX = 0;
+            }
+
+
+        }
+
         private void generateFruit()
         {
             
@@ -63,10 +115,46 @@ namespace Snake
             this.Controls.Add(fruit);
         }
 
+        private void _generateMap()
+        {
+            for (int i = 0; i < _width/_sizeOfSides - 4; i++)
+            {
+                PictureBox pic = new PictureBox();
+                pic.BackColor = Color.Black;
+                pic.Location = new Point(0, _sizeOfSides * i);
+                pic.Size = new Size(_width - 100, 1);
+                this.Controls.Add(pic);
+            }
+            for (int i = 0; i <= _height / _sizeOfSides; i++)
+            {
+                PictureBox pic = new PictureBox();
+                pic.BackColor = Color.Black;
+                pic.Location = new Point(_sizeOfSides * i,0);
+                pic.Size = new Size(1, _width - 100);
+                this.Controls.Add(pic);
+            }
+
+        }
+
+        private void eatItSelf()
+        {
+            for (int i = 1; i < score; i++)
+            {
+                if (snake[0].Location == snake[i].Location)
+                {
+                  
+                        timer.Stop();
+                        MessageBox.Show("Game over! Your Score is:" + score);
+                     
+                }
+            }
+        }
+
         private void eatFruit()
         {
             if(snake[0].Location.X == rX && snake[0].Location.Y == rY)
             {
+                
                 PictureBox cube = new PictureBox();
                 cube.Location = new Point(snake[snake.Count - 1].Location.X + _sizeOfSides * dirX, snake[snake.Count - 1].Location.Y + _sizeOfSides * dirY);
                 cube.Size = new Size(_sizeOfSides, _sizeOfSides);
@@ -74,14 +162,19 @@ namespace Snake
                 snake.Add(cube);
                 this.Controls.Add(cube);
                 timer.Interval -= 10;
+                labelScore.Text = "Score: " + ++score;
+                
                 generateFruit();
+
             }
         }
 
 
         private void update(object sender, EventArgs e)
         {
+            eatItSelf();
             eatFruit();
+            checkBorders();
             moveSnake();
         }
 
@@ -97,22 +190,22 @@ namespace Snake
         private void keyDown_pressed(object sender, KeyEventArgs e)
         {
             string keyCode = e.KeyCode.ToString();
-            if (keyCode == "Right")
+            if (keyCode == "Right" && dirX != -1)
             {
                 dirX = 1;
                 dirY = 0;
             }
-            else if (keyCode == "Left")
+            else if (keyCode == "Left" && dirX != 1)
             {
                 dirX = -1;
                 dirY = 0;
             }
-            else if (keyCode == "Up")
+            else if (keyCode == "Up" && dirY != 1)
             {
                 dirX = 0;
                 dirY = -1;
             }
-            else if (keyCode == "Down")
+            else if (keyCode == "Down" && dirY != -1)
             {
                 dirX = 0;
                 dirY = 1;
@@ -120,13 +213,6 @@ namespace Snake
 
         }
 
-        private void button_Click(object sender, EventArgs e)
-        {
-            Button button = sender as Button;
-
-            button.Text = button.Tag + " HELLO";
-
-        }
 
        
     }
